a2b4ac2fc47095132e7f665d44ec145df9380817
Henry Kupty <hkupty@gmail.com>
2024-11-11 17:27:33 +0100

    feat: Pass flags

--- cli/main.go
+++ cli/main.go
@@ -4,12 +4,32 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/alexflint/go-arg"
 	"github.com/hkupty/joule-reader/pkg"
 )
 
+var args struct {
+	FailuresOrErrors bool     `arg:"-x"`
+	Files            []string `arg:"positional"`
+}
+
 func main() {
-	for _, arg := range os.Args[1:] {
+	ret := 0
+	arg.MustParse(&args)
+	for _, arg := range args.Files {
 		f := pkg.ReadFile(arg)
-		fmt.Println(f)
+		hasFailures := f.Failures > 0
+		hasErrors := f.Errors > 0
+
+		switch {
+		case args.FailuresOrErrors && (hasFailures || hasErrors):
+			ret += 1
+			fmt.Println(f)
+		case !args.FailuresOrErrors:
+			fmt.Println(f)
+		}
+
 	}
+
+	os.Exit(ret)
 }
